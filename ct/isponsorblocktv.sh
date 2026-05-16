c7a4b7516f1006b34e770d9d9900bf1525356254
Matthias Schabhüttl <63157170+mschabhuettl@users.noreply.github.com>
2026-05-16 18:58:03 +0200

    Update isponsorblocktv.sh

--- ct/isponsorblocktv.sh
+++ ct/isponsorblocktv.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/mschabhuettl/ProxmoxVE/main/misc/build.func)
 
 # Copyright (c) 2021-2026 community-scripts ORG
 # Author: Matthew Stern (sternma) | MickLesk (CanbiZ)
@@ -10,7 +10,7 @@ APP="iSponsorBlockTV"
 var_tags="${var_tags:-media;automation}"
 var_cpu="${var_cpu:-1}"
 var_ram="${var_ram:-1024}"
-var_disk="${var_disk:-4}"
+var_disk="${var_disk:-16}"
 var_os="${var_os:-debian}"
 var_version="${var_version:-13}"
 var_unprivileged="${var_unprivileged:-1}"
